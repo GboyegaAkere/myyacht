4e5ff2b0cd0fc4492b0f869000f937b7f744917b
GboyegaAkere <oluwagboyegaakeremale80@gmail.com>
2025-09-13 14:03:25 +0100

    new commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Home from './pages/Home'
-import ScrollToTop from './components/ScrollToTop'
+import React from "react";
+import Home from "./pages/Home";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
-    <>
-    <Home/>
-    <ScrollToTop/>
-    </>
-  )
-}
+    <div className="w-full overflow-x-hidden">
+      {/* your whole app */}
+      <Home />
+      <ScrollToTop />
+    </div>
+  );
+};
 
-export default App
+export default App;
